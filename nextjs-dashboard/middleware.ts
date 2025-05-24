f81cfc048e433661ee8007b16e0c41e1212ebf0f
miranda-beatriz <miranda.beatrizcosta@gmail.com>
2025-05-24 20:29:17 -0300

    fixing 500 error

--- nextjs-dashboard/middleware.ts
+++ nextjs-dashboard/middleware.ts
@@ -1,9 +1,18 @@
-import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
+// here I had to change the tutorial code because in the url didn´t work but I kept to show that I followed the tutorial
+//import NextAuth from 'next-auth';
+//import { authConfig } from './auth.config';
  
-export default NextAuth(authConfig).auth;
+//export default NextAuth(authConfig).auth;
  
-export const config = {
+//export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+  //matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+//};
+
+import { auth } from './auth';
+
+export default auth;
+
+export const config = {
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
