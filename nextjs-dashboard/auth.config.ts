d22af5984f71610422c6b8863927dc149ab37737
miranda-beatriz <miranda.beatrizcosta@gmail.com>
2025-05-24 20:21:18 -0300

    fixed the 505 error

--- nextjs-dashboard/auth.config.ts
+++ nextjs-dashboard/auth.config.ts
@@ -1,5 +1,25 @@
 import type { NextAuthConfig } from 'next-auth';
- 
+ // this part it´s from tutorial which vercel didn´t worked
+//export const authConfig = {
+  //pages: {
+    //signIn: '/login',
+  //},
+  //callbacks: {
+    //authorized({ auth, request: { nextUrl } }: any) {
+      //const isLoggedIn = !!auth?.user;
+      //const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      //if (isOnDashboard) {
+        //if (isLoggedIn) return true;
+       // return false; // Redirect unauthenticated users to login page
+      //} else if (isLoggedIn) {
+        //return Response.redirect(new URL('/dashboard', nextUrl));
+      //}
+      //return true;
+    //},
+  //},
+  //providers: [], // Add providers with an empty array for now
+//} satisfies NextAuthConfig;
+
 export const authConfig = {
   pages: {
     signIn: '/login',
@@ -8,14 +28,15 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }: any) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+
       if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
+        // Bloqueia se não estiver logado → redireciona automaticamente para /login
+        return isLoggedIn;
       }
+
+      // Fora do dashboard, permite acesso sempre
       return true;
     },
   },
-  providers: [], // Add providers with an empty array for now
+  providers: [], // Pode adicionar provedores depois
 } satisfies NextAuthConfig;
