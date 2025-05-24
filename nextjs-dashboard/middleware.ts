b2bd55a3f11746486fa6c25a8155e9ffbc32aa8c
miranda-beatriz <miranda.beatrizcosta@gmail.com>
2025-05-24 20:31:21 -0300

    returned because now vercel didn´t accepted

--- nextjs-dashboard/middleware.ts
+++ nextjs-dashboard/middleware.ts
@@ -1,18 +1,10 @@
-// here I had to change the tutorial code because in the url didn´t work but I kept to show that I followed the tutorial
-//import NextAuth from 'next-auth';
-//import { authConfig } from './auth.config';
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
  
-//export default NextAuth(authConfig).auth;
+export default NextAuth(authConfig).auth;
  
-//export const config = {
-  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  //matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
-//};
-
-import { auth } from './auth';
-
-export default auth;
-
 export const config = {
+  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
+
